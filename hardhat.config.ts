e725842e160ff4b866434c9d1a733af9d73fd02b
taaaaho <taaaaho@gmail.com>
2022-05-10 21:10:54 +0900

    feat: typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/types";
 
 import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: "0.8.9", settings: {} }],
