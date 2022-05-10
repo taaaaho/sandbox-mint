8f8ad8c234f2d01b225c23ca3d86d270bd85b631
taaaaho <taaaaho@gmail.com>
2022-05-10 19:33:53 +0900

    design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Text, VStack } from '@chakra-ui/react'
+import { Box, Center, Text, VStack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { MintButton } from '../component/organisms/MintButton'
@@ -12,13 +12,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Center verticalAlign="center" h="100vh">
-        <VStack>
-          <Text>This is Rinkeby Testnet NFT mint page</Text>
-          <Text>You can receive NFT by free</Text>
-          <MintButton />
-        </VStack>
-      </Center>
+      <Box
+        backgroundImage={'https://source.unsplash.com/random'}
+        backgroundPosition="center"
+        backgroundSize="cover"
+      >
+        <Center>
+          <VStack
+            bg="rgba(0,0,0,0.3)"
+            h="100vh"
+            w="100vw"
+            justifyContent="center"
+          >
+            <Text color="white" fontWeight="semibold">
+              This is Rinkeby Testnet NFT mint page
+            </Text>
+            <Text color="white" fontWeight="semibold">
+              You can receive NFT by free
+            </Text>
+            <MintButton />
+          </VStack>
+        </Center>
+      </Box>
     </>
   )
 }
